94ef96467e162e07bacd0b4180becb9a6492f8b5
adamskopl <adam@skobo.pl>
2015-05-23 15:17:20 +0200

    generate_min.sh - script combinig all .js file into safedirve.combines.js, which is ready for compression

---
+++ generate_min.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+MIN_FILE="./safedrive.combined.js"
+
+echo "removing $MIN_FILE"
+rm $MIN_FILE
+
+echo "including license in $MIN_FILE"
+cat ./license.txt >> $MIN_FILE
+
+echo "include constants file (it has to be first)"
+cat ./src/constants.js >> $MIN_FILE
+
+echo "including *.js files (except those included first) from 'src' in minify.js" 
+find src -iname "*.js" ! -iname "constants.js" -exec cat {} >> $MIN_FILE \;
